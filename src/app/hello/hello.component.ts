78e03a6810bc67de78fa0826ad3b2e00742a8961
Carlos Mejia <cmejia@equitel.com.co>
2022-09-14 19:13:49 -0500

    input decorator

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'custom-hello',
@@ -7,10 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class HelloComponent {
-  public name: string = null;
+  @Input() name: string = null;
 
   constructor() {
-    this.name = 'Enrique';
   }
 
 }

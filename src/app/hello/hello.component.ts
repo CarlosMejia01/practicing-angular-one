adcf1124c599d45b9b84a8a635c9a495bceb2edb
Carlos Mejia <cmejia@equitel.com.co>
2022-09-15 00:00:28 -0500

    binding bi-directional

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -8,13 +8,29 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 export class HelloComponent {
   @Input() name: string = null;
-  @Output() sayHello:EventEmitter<string> = new EventEmitter<string>();
+  @Output() sayHello: EventEmitter<string> = new EventEmitter<string>();
+
+  inputName: string = 'Jhon';
+
+  nameList: Array<string> = [];
+
 
   constructor() {
   }
 
   onNameClick() {
     this.sayHello.emit('Hello!');
+  }
+
+  addName() {
+    this.nameList.push(this.inputName);
+    this.inputName = '';
+    console.log(this.nameList);
+  }
+
+  updateInputName(event: any) {
+    this.inputName = event.target.value;
+    console.log(this.inputName);
   }
 
 }
